57215e9af2782940104164b4cd71e7abb90185e1
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-19 21:54:18 -0600

    removed endpoints

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,7 +12,6 @@ import express, { Request, Response, Application } from 'express';
 import { corsOptions } from "./configs/cors.config.js"
 import cors from "cors";
 import { RegisterRoutes } from "./routes.js";
-import path from "node:path";
 import { swaggerOptions } from "./configs/swagger.config.js";
 import swaggerJson from "./swagger.json" with { type: 'json' };
 import * as swaggerUI from "swagger-ui-express";
@@ -29,27 +28,10 @@ export const buildApp = (): Application => {
   app.use(cors(corsOptions));
 
 
-  app.get('/home', (req: Request, res: Response) => {
-    res.send('Hello World with TypeScript and Express!');
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the MikroORM tutorial API!');
   });
   
-  app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'get received successfully' });
-  });
-
-  app.post('/', (req: Request, res: Response) => {
-    res.json({ message: 'post received successfully' });
-  });
-
-  app.delete('/', (req: Request, res: Response) => {
-    res.json({ message: 'delete received successfully' });
-  });
-
-  app.put('/', (req: Request, res: Response) => {
-    res.json({ message: 'put received successfully' });
-  });
-
-
   RegisterRoutes(app);
   
   app.use(["/openapi", "/docs", "/swagger"], swaggerUI.serve, swaggerUI.setup(swaggerJson));
